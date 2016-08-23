a451786838a821f4fe98ec4e657c62698ff48939
Chad Moon <cmoon@kenzan.com>
2016-08-23 11:05:02 -0600

    build deck during clone stage

--- clone.py
+++ clone.py
@@ -3,9 +3,9 @@ import os
 def cmd (cmd):
     os.system(cmd)
 
-services = ('front50', 'clouddriver', 'orca', 'gate')
+services = ('front50', 'clouddriver', 'orca', 'rosco', 'gate')
 
-
+cmd("rm -rf services")
 cmd("mkdir services")
 
 for service in services:
@@ -14,3 +14,7 @@ for service in services:
     cmd("cp config/" + service + ".yml services/" + service + "/" + service + "-web/config/" + service + ".yml")
 
 cmd("git clone https://github.com/spinnaker/deck.git services/deck")
+
+print "building deck"
+cmd('docker run --rm -v `pwd`/services/deck:/build -e AUTH_ENABLED=false -e API_HOST=/gate -e BAKERY_DETAIL_URL=/bakery quay.io/spinnaker/deck bash -c "cd /build; npm install; npm run build"')
+print "deck built inside services/deck/build/webpack"
